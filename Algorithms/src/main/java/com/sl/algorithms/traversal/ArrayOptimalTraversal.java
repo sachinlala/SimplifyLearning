82ef413784482dd8c4d554272b6962ff2c66fafb
Sachin Lala <sachin.lala@gmail.com>
2017-11-13 21:13:24 +0530

    Added variants for maximum sub-array problem and condensed unit-tests

--- Algorithms/src/main/java/com/sl/algorithms/traversal/ArrayOptimalTraversal.java
+++ Algorithms/src/main/java/com/sl/algorithms/traversal/ArrayOptimalTraversal.java
@@ -95,9 +95,7 @@ public class ArrayOptimalTraversal {
             return start;
         }
         int mid = Formulas.midPoint(start, end);
-        System.out.println(mid);
         if (arr[mid]-arr[start] != (mid-start)) {
-            System.out.println(mid);
             return findSmallestMissingNumberSorted(arr, start, mid);
         }
         return findSmallestMissingNumberSorted(arr, mid+1, end);
@@ -171,5 +169,47 @@ public class ArrayOptimalTraversal {
             }
         }
         return maxContiguousSum;
+    }
+
+    //O(n)
+    public static int findMaxNonNeighboursSumSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int maxSum = 0;
+        int maxLeftSidePrevSum = nums[0];
+        int maxLeftSideSum = nums[1];
+        for (int i=2; i<nums.length; i++) {
+            maxLeftSidePrevSum += nums[i];
+            maxSum = Math.max(maxLeftSideSum, maxLeftSidePrevSum);
+            maxLeftSidePrevSum = maxLeftSideSum;
+            maxLeftSideSum = maxSum;
+        }
+        return maxSum;
+    }
+
+    public static int findMaxSubSequenceSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int maxSum = Integer.MIN_VALUE;
+        int maxNum = nums[0];
+        for (int num : nums) {
+            if (num > 0) {
+                maxSum += num;
+            }
+            maxNum = Math.max(maxSum, num);
+            maxSum = Math.max(maxSum, maxNum);
+        }
+        return maxSum;
     }
 }
--- Algorithms/src/test/java/com/sl/algorithms/traversal/ArrayOptimalTraversalTest.java
+++ Algorithms/src/test/java/com/sl/algorithms/traversal/ArrayOptimalTraversalTest.java
@@ -4,56 +4,20 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static com.sl.algorithms.traversal.ArrayTraversal.printArray;
 import static com.sl.algorithms.traversal.ArrayOptimalTraversal.*;
+import static com.sl.algorithms.traversal.ArrayTraversal.printArray;
 
 public class ArrayOptimalTraversalTest {
     @Test
-    public void testRotationLeftByJuggling1() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[23451]", printArray(rotateLeftByJuggling(a, 1)));
-    }
-
-    @Test
-    public void testRotationLeftByJuggling2() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[34512]", printArray(rotateLeftByJuggling(a, 2)));
-    }
-
-    @Test
-    public void testRotationLeftByJuggling3() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[45123]", printArray(rotateLeftByJuggling(a, 3)));
-    }
-
-    @Test
-    public void testRotationLeftByJuggling4() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[51234]", printArray(rotateLeftByJuggling(a, 4)));
-    }
-
-    @Test
-    public void testRotationLeftByJuggling5() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[12345]", printArray(rotateLeftByJuggling(a, 5)));
-    }
-
-    @Test
-    public void testRotationLeftByJuggling6() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[23451]", printArray(rotateLeftByJuggling(a, 6)));
-    }
-
-    @Test
-    public void testRotationLeftByJuggling7() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[34512]", printArray(rotateLeftByJuggling(a, 7)));
-    }
-
-    @Test
-    public void testRotationLeftByJugglingHCF() {
-        int[] a = {1, 2, 3, 4};
-        Assert.assertEquals("[3412]", printArray(rotateLeftByJuggling(a, 2)));
+    public void testRotationLeftByJuggling() {
+        Assert.assertEquals("[23451]", printArray(rotateLeftByJuggling(new int[]{1, 2, 3, 4, 5}, 1)));
+        Assert.assertEquals("[34512]", printArray(rotateLeftByJuggling(new int[]{1, 2, 3, 4, 5}, 2)));
+        Assert.assertEquals("[45123]", printArray(rotateLeftByJuggling(new int[]{1, 2, 3, 4, 5}, 3)));
+        Assert.assertEquals("[51234]", printArray(rotateLeftByJuggling(new int[]{1, 2, 3, 4, 5}, 4)));
+        Assert.assertEquals("[12345]", printArray(rotateLeftByJuggling(new int[]{1, 2, 3, 4, 5}, 5)));
+        Assert.assertEquals("[23451]", printArray(rotateLeftByJuggling(new int[]{1, 2, 3, 4, 5}, 6)));
+        Assert.assertEquals("[34512]", printArray(rotateLeftByJuggling(new int[]{1, 2, 3, 4, 5}, 7)));
+        Assert.assertEquals("[3412]", printArray(rotateLeftByJuggling(new int[]{1, 2, 3, 4}, 2)));
     }
 
     @Test
@@ -65,220 +29,113 @@ public class ArrayOptimalTraversalTest {
     }
 
     @Test
-    public void testRotationLeftByReversal1() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[23451]", printArray(rotateLeftByReversal(a, 1)));
+    public void testRotationLeftByReversal() {
+        Assert.assertEquals("[23451]", printArray(rotateLeftByReversal(new int[]{1, 2, 3, 4, 5}, 1)));
+        Assert.assertEquals("[34512]", printArray(rotateLeftByReversal(new int[]{1, 2, 3, 4, 5}, 2)));
+        Assert.assertEquals("[45123]", printArray(rotateLeftByReversal(new int[]{1, 2, 3, 4, 5}, 3)));
+        Assert.assertEquals("[12345]", printArray(rotateLeftByReversal(new int[]{1, 2, 3, 4, 5}, 5)));
+        Assert.assertEquals("[51234]", printArray(rotateLeftByReversal(new int[]{1, 2, 3, 4, 5}, 4)));
+        Assert.assertEquals("[23451]", printArray(rotateLeftByReversal(new int[]{1, 2, 3, 4, 5}, 6)));
+        Assert.assertEquals("[34512]", printArray(rotateLeftByReversal(new int[]{1, 2, 3, 4, 5}, 7)));
     }
 
     @Test
-    public void testRotationLeftByReversal2() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[34512]", printArray(rotateLeftByReversal(a, 2)));
-    }
-
-    @Test
-    public void testRotationLeftByReversal3() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[45123]", printArray(rotateLeftByReversal(a, 3)));
-    }
-
-    @Test
-    public void testRotationLeftByReversal4() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[51234]", printArray(rotateLeftByReversal(a, 4)));
-    }
-
-    @Test
-    public void testRotationLeftByReversal5() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[12345]", printArray(rotateLeftByReversal(a, 5)));
-    }
-
-    @Test
-    public void testRotationLeftByReversal6() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[23451]", printArray(rotateLeftByReversal(a, 6)));
-    }
-
-    @Test
-    public void testRotationLeftByReversal7() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[34512]", printArray(rotateLeftByReversal(a, 7)));
-    }
-
-    @Test
-    public void testRotationRightByReversal1() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[51234]", printArray(rotateRightByReversal(a, 1)));
-    }
-
-    @Test
-    public void testRotationRightByReversal2() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[45123]", printArray(rotateRightByReversal(a, 2)));
-    }
-
-    @Test
-    public void testRotationRightByReversal3() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[34512]", printArray(rotateRightByReversal(a, 3)));
-    }
-
-    @Test
-    public void testRotationRightByReversal4() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[23451]", printArray(rotateRightByReversal(a, 4)));
-    }
-
-    @Test
-    public void testRotationRightByReversal5() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[12345]", printArray(rotateRightByReversal(a, 5)));
-    }
-
-    @Test
-    public void testRotationRightByReversal6() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[51234]", printArray(rotateRightByReversal(a, 6)));
-    }
-
-    @Test
-    public void testRotationRightByReversal7() {
-        int[] a = {1, 2, 3, 4, 5};
-        Assert.assertEquals("[45123]", printArray(rotateRightByReversal(a, 7)));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberSortedEmpty() {
-        int[] a = {};
-        Assert.assertEquals(0, findSmallestMissingNumberSorted(a, 0, 0));
-        Assert.assertEquals(0, findSmallestMissingNumberSorted(null, 0, 0));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberSorted() {
-        int[] a = {0, 1, 2, 4, 5};
-        Assert.assertEquals(3, findSmallestMissingNumberSorted(a, 0, 4));
+    public void testRotationRightByReversal() {
+        Assert.assertEquals("[51234]", printArray(rotateRightByReversal(new int[]{1, 2, 3, 4, 5}, 1)));
+        Assert.assertEquals("[45123]", printArray(rotateRightByReversal(new int[]{1, 2, 3, 4, 5}, 2)));
+        Assert.assertEquals("[34512]", printArray(rotateRightByReversal(new int[]{1, 2, 3, 4, 5}, 3)));
+        Assert.assertEquals("[23451]", printArray(rotateRightByReversal(new int[]{1, 2, 3, 4, 5}, 4)));
+        Assert.assertEquals("[12345]", printArray(rotateRightByReversal(new int[]{1, 2, 3, 4, 5}, 5)));
+        Assert.assertEquals("[51234]", printArray(rotateRightByReversal(new int[]{1, 2, 3, 4, 5}, 6)));
+        Assert.assertEquals("[45123]", printArray(rotateRightByReversal(new int[]{1, 2, 3, 4, 5}, 7)));
     }
 
     @Test
     public void testFindSmallestMissingNumberSortedXOR() {
-        int[] a = {0, 1, 2, 4, 5};
-        Assert.assertEquals(3, findSmallestMissingNumberSortedXOR(a));
+        Assert.assertEquals(3, findSmallestMissingNumberSortedXOR(new int[]{0, 1, 2, 4, 5}));
+    }
+
+    @Test
+    public void testFindSmallestMissingNumberSorted() {
+        Assert.assertEquals(0, findSmallestMissingNumberSorted(new int[]{}, 0, 0));
+        Assert.assertEquals(0, findSmallestMissingNumberSorted(null, 0, 0));
+        Assert.assertEquals(3, findSmallestMissingNumberSorted(new int[]{0, 1, 2, 4, 5}, 0, 4));
+        Assert.assertEquals(0, findSmallestMissingNumberSorted(new int[]{1, 2, 4, 5}, 0, 3));
+        Assert.assertEquals(6, findSmallestMissingNumberSorted(new int[]{0, 1, 2, 3, 4, 5}, 0, 5));
     }
 
     @Ignore
     @Test
-    public void testFindSmallestMissingNumberSortedDup1() {
-        int[] a = {0, 0, 1, 2, 4, 5};
-        Assert.assertEquals(3, findSmallestMissingNumberSortedXOR(a));
-    }
-
-    @Ignore
-    @Test
-    public void testFindSmallestMissingNumberSortedDup2() {
-        int[] a = {0, 0, 1, 2, 2, 4, 5};
-        Assert.assertEquals(3, findSmallestMissingNumberSortedXOR(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberSortedNoZero() {
-        int[] a = {1, 2, 4, 5};
-        Assert.assertEquals(0, findSmallestMissingNumberSorted(a, 0, 3));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberSortedContiguous() {
-        int[] a = {0, 1, 2, 3, 4, 5};
-        Assert.assertEquals(6, findSmallestMissingNumberSorted(a, 0, 5));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsortedNegative1() {
-        int[] a = {-1, 1, 2, 3};
-        Assert.assertEquals(4, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsortedNegative2() {
-        int[] a = {-2, -3, -4, -8};
-        Assert.assertEquals(1, findSmallestMissingNumberUnsorted(a));
+    public void testFindSmallestMissingNumberSortedDup() {
+        Assert.assertEquals(3, findSmallestMissingNumberSortedXOR(new int[]{0, 0, 1, 2, 2, 4, 5}));
+        Assert.assertEquals(3, findSmallestMissingNumberSortedXOR(new int[]{0, 0, 1, 2, 4, 5}));
     }
 
     @Test
     public void testFindSmallestMissingNumberUnsorted() {
-        int[] a = {1, 2, 4, 5};
-        Assert.assertEquals(3, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsorted1() {
-        int[] a = {0, 1, 2, 3};
-        Assert.assertEquals(4, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsorted2() {
-        int[] a = {0, 1, 2, 4};
-        Assert.assertEquals(3, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsorted3() {
-        int[] a = {4, 1, 2, 3};
-        Assert.assertEquals(5, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsorted4() {
-        int[] a = {4, 2, 1, 3};
-        Assert.assertEquals(5, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsorted5() {
-        int[] a = {4, 2, 3, 1};
-        Assert.assertEquals(5, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsorted6() {
-        int[] a = {3, 4, -1, 1};
-        Assert.assertEquals(2, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsorted7() {
-        int[] a = {4, 3, 2, 1};
-        Assert.assertEquals(5, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsortedOutlier() {
-        int[] a = {40, 1, 2, 3};
-        Assert.assertEquals(4, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsortedDup() {
-        int[] a = {4, 1, 4, 3};
-        Assert.assertEquals(2, findSmallestMissingNumberUnsorted(a));
-    }
-
-    @Test
-    public void testFindSmallestMissingNumberUnsortedEmpty() {
-        int[] a = {};
-        Assert.assertEquals(1, findSmallestMissingNumberUnsorted(a));
+        Assert.assertEquals(4, findSmallestMissingNumberUnsorted(new int[]{-1, 1, 2, 3}));
+        Assert.assertEquals(1, findSmallestMissingNumberUnsorted(new int[]{-2, -3, -4, -8}));
+        Assert.assertEquals(3, findSmallestMissingNumberUnsorted(new int[]{1, 2, 4, 5}));
+        Assert.assertEquals(4, findSmallestMissingNumberUnsorted(new int[]{0, 1, 2, 3}));
+        Assert.assertEquals(3, findSmallestMissingNumberUnsorted(new int[]{0, 1, 2, 4}));
+        Assert.assertEquals(5, findSmallestMissingNumberUnsorted(new int[]{4, 1, 2, 3}));
+        Assert.assertEquals(5, findSmallestMissingNumberUnsorted(new int[]{4, 2, 1, 3}));
+        Assert.assertEquals(5, findSmallestMissingNumberUnsorted(new int[]{4, 2, 3, 1}));
+        Assert.assertEquals(2, findSmallestMissingNumberUnsorted(new int[]{3, 4, -1, 1}));
+        Assert.assertEquals(5, findSmallestMissingNumberUnsorted(new int[]{4, 3, 2, 1}));
+        Assert.assertEquals(4, findSmallestMissingNumberUnsorted(new int[]{40, 1, 2, 3}));
+        Assert.assertEquals(2, findSmallestMissingNumberUnsorted(new int[]{4, 1, 4, 3}));
+        Assert.assertEquals(1, findSmallestMissingNumberUnsorted(new int[]{}));
     }
 
     @Test
     public void testFindMaxContiguousSumSubArray() {
+        Assert.assertEquals(15, findMaxContiguousSumSubArray(new int[]{1, 2, 3, 4, 5}));
         Assert.assertEquals(0, findMaxContiguousSumSubArray(null));
         Assert.assertEquals(0, findMaxContiguousSumSubArray(new int[]{}));
         Assert.assertEquals(1, findMaxContiguousSumSubArray(new int[]{1}));
+        Assert.assertEquals(3, findMaxContiguousSumSubArray(new int[]{1, 2}));
         Assert.assertEquals(-1, findMaxContiguousSumSubArray(new int[]{-1}));
+        Assert.assertEquals(-1, findMaxContiguousSumSubArray(new int[]{-1, -2}));
+        Assert.assertEquals(1, findMaxContiguousSumSubArray(new int[]{-1, -2, 1}));
         Assert.assertEquals(5, findMaxContiguousSumSubArray(new int[]{-1, 2, 3}));
         Assert.assertEquals(2, findMaxContiguousSumSubArray(new int[]{-10, 2, -5}));
         Assert.assertEquals(4, findMaxContiguousSumSubArray(new int[]{-1, 2, -1, 3}));
     }
+
+    @Test
+    public void testFindMaxSubSequenceSum() {
+        Assert.assertEquals(15, findMaxSubSequenceSum(new int[]{1, 2, 3, 4, 5}));
+        Assert.assertEquals(0, findMaxSubSequenceSum(null));
+        Assert.assertEquals(0, findMaxSubSequenceSum(new int[]{}));
+        Assert.assertEquals(1, findMaxSubSequenceSum(new int[]{1}));
+        Assert.assertEquals(3, findMaxSubSequenceSum(new int[]{1, 2}));
+        Assert.assertEquals(-1, findMaxSubSequenceSum(new int[]{-1}));
+        Assert.assertEquals(-1, findMaxSubSequenceSum(new int[]{-1, -2}));
+        Assert.assertEquals(1, findMaxSubSequenceSum(new int[]{-1, -2, 1}));
+        Assert.assertEquals(5, findMaxSubSequenceSum(new int[]{-1, 2, 3}));
+        Assert.assertEquals(2, findMaxSubSequenceSum(new int[]{-10, 2, -5}));
+        Assert.assertEquals(5, findMaxSubSequenceSum(new int[]{-1, 2, -1, 3}));
+    }
+
+    @Test
+    public void testFindMaxNonNeighboursSumSubArray() {
+        Assert.assertEquals(9, findMaxNonNeighboursSumSubArray(new int[]{1, 2, 3, 4, 5}));
+        Assert.assertEquals(0, findMaxNonNeighboursSumSubArray(null));
+        Assert.assertEquals(0, findMaxNonNeighboursSumSubArray(new int[]{}));
+        Assert.assertEquals(1, findMaxNonNeighboursSumSubArray(new int[]{1}));
+        Assert.assertEquals(2, findMaxNonNeighboursSumSubArray(new int[]{1, 2}));
+        Assert.assertEquals(4, findMaxNonNeighboursSumSubArray(new int[]{1, 2, 3}));
+        Assert.assertEquals(27, findMaxNonNeighboursSumSubArray(new int[]{7, 8, 20}));
+        Assert.assertEquals(25, findMaxNonNeighboursSumSubArray(new int[]{2, 5, 10, 20}));
+    }
 }
+
+
+
+
+
+
+
+
+
