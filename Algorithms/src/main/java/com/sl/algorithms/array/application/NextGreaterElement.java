db934800e75e700e9d9412d030968847b02fcec0
Sachin Lala <sachin.lala@gmail.com>
2017-12-04 11:39:31 +0530

    Added NGE assertion for Circular Array

--- Algorithms/src/main/java/com/sl/algorithms/array/application/NextGreaterElement.java
+++ Algorithms/src/main/java/com/sl/algorithms/array/application/NextGreaterElement.java
@@ -1,8 +1,14 @@
 package com.sl.algorithms.array.application;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
+/**
+ * <br><a href="https://leetcode.com/problems/next-greater-element-i/description/">Variant 1</a>
+ * <br><a href="https://leetcode.com/problems/next-greater-element-ii/description/">Variant 2</a>
+ * <br><a href="">Variant 3</a>
+ */
 public class NextGreaterElement {
     private NextGreaterElement() {
         /**
@@ -27,11 +33,13 @@ public class NextGreaterElement {
         }
         return warmTemperatures;
     }
+}
 
-    /**
-     * <a href="https://leetcode.com/contest/weekly-contest-61/problems/daily-temperatures/">Daily Temperatures</a><br>
-     * // O(n) time and O(n) space
-     */
+/**
+ * <a href="https://leetcode.com/contest/weekly-contest-61/problems/daily-temperatures/">Daily Temperatures</a><br>
+ */
+class NGERegularArray {
+    // O(n) time and O(n) space
     public static int[] findDaysToWarmth(int[] temperatures) {
         int n = temperatures.length;
         int[] warmTemperatures = new int[n]; // => each element = 0
@@ -40,14 +48,40 @@ public class NextGreaterElement {
             while (!ngeStack.isEmpty()) { // search till the stack is empty
                 int topIndex = ngeStack.peek();
                 if (temperatures[i] < temperatures[topIndex]) {
-                    warmTemperatures[i] = (topIndex-i);
+                    warmTemperatures[i] = (topIndex - i);
+                    break; // :-)
+                }
+                ngeStack.pop();
+            }
+            ngeStack.push(i);
+        }
+        return warmTemperatures;
+    }
+}
+
+/**
+ * <a href="https://leetcode.com/problems/next-greater-element-ii/description/">Variant 2</a>
+ */
+class NGECircularArray {
+    // O(n) time and O(n) space
+    public static int[] findNGEInCircularArray(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+        Arrays.fill(output, -1); // O(1)
+        Deque<Integer> ngeStack = new ArrayDeque<>(); // O(n)
+        for (int i=2*n-1; i>=0; i--) { // O(n)
+            int index = i%n;
+            while (!ngeStack.isEmpty()) {
+                int topIndex = ngeStack.peek();
+                if (nums[index] < nums[topIndex]) {
+                    output[index] = nums[topIndex];
                     break;
                 } else {
                     ngeStack.pop();
                 }
             }
-            ngeStack.push(i);
+            ngeStack.push(index);
         }
-        return warmTemperatures;
+        return output;
     }
 }
