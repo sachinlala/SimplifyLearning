b654f3ed75181cd5244cf0e0c8c5aa901b52643c
Sachin Lala <sachin.lala@gmail.com>
2017-12-18 11:50:51 +0530

    Added LinkedList -> SwapInPairs recursive algorithm

---
+++ Algorithms/src/main/java/com/sl/algorithms/linear/linkedlist/SwapInPairs.java
@@ -0,0 +1,20 @@
+package com.sl.algorithms.linear.linkedlist;
+
+import com.sl.algorithms.core.linear.ListNode;
+
+public class SwapInPairs {
+
+    SwapInPairs() {
+        /**
+         * This is a utility class.<br>
+         */
+    }
+
+    public static ListNode<Integer> swapInPairs(ListNode<Integer> head) {
+        if (head == null || head.next == null) return head;
+        ListNode<Integer> next = head.next;
+        head.next = swapInPairs(head.next.next);
+        next.next = head;
+        return next;
+    }
+}
---
+++ Algorithms/src/test/java/com/sl/algorithms/linear/linkedlist/SwapInPairsTest.java
@@ -0,0 +1,38 @@
+package com.sl.algorithms.linear.linkedlist;
+
+import com.sl.algorithms.core.linear.LinkedListOps;
+import com.sl.algorithms.core.linear.ListNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SwapInPairsTest extends SwapInPairs {
+
+    @Test
+    public void testSwapInPairsBaseCases() {
+        Assert.assertNull(swapInPairs(null));
+
+        ListNode<Integer> singleNode = new ListNode<>(1);
+        Assert.assertEquals(swapInPairs(singleNode), singleNode);
+    }
+
+    @Test
+    public void testSwapInPairs2Elements() {
+        ListNode<Integer> pair = LinkedListOps.createLinkedList(new int[]{1,2});
+        ListNode<Integer> reverse = LinkedListOps.createLinkedList(new int[]{2,1});
+        Assert.assertEquals(swapInPairs(pair), reverse);
+    }
+
+    @Test
+    public void testSwapInPairsEvenCount() {
+        ListNode<Integer> list = LinkedListOps.createLinkedList(new int[]{1,2,3,4});
+        ListNode<Integer> result = LinkedListOps.createLinkedList(new int[]{2,1,4,3});
+        Assert.assertEquals(swapInPairs(list), result);
+    }
+
+    @Test
+    public void testSwapInPairsOddCount() {
+        ListNode<Integer> list = LinkedListOps.createLinkedList(new int[]{1,2,3,4,5});
+        ListNode<Integer> result = LinkedListOps.createLinkedList(new int[]{2,1,4,3,5});
+        Assert.assertEquals(swapInPairs(list), result);
+    }
+}
