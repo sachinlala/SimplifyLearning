e3b6ce2d8e0f19e7a68bc2ab6d0f1e7d6438de85
Sachin Lala <sachin.lala@gmail.com>
2018-02-21 16:19:40 +0530

    Added findLCA for BST and closed #25

--- Kihon/src/test/java/com/sl/algorithms/core/tree/lca/LCAFinderTest.java
+++ Kihon/src/test/java/com/sl/algorithms/core/tree/lca/LCAFinderTest.java
@@ -16,7 +16,11 @@ public class LCAFinderTest {
         testNull();
         testSimpleTreeLCA();
         testParentChild();
+        testOnlyLeft();
+        testOnlyRight();
         testBST();
+        testBSTLeft();
+        testBSTRight();
     }
 
     @Test
@@ -26,7 +30,11 @@ public class LCAFinderTest {
         testNull();
         testSimpleTreeLCA();
         testParentChild();
+        testOnlyLeft();
+        testOnlyRight();
         testBST();
+        testBSTLeft();
+        testBSTRight();
     }
 
     private void testNull() {
@@ -50,6 +58,22 @@ public class LCAFinderTest {
         Assert.assertEquals(bTree, lowestCommonAncestorS.findLCA(bTree, bTree.left, bTree));
     }
 
+    private void testOnlyLeft() {
+        TreeNode<Integer> bTree = new TreeNode<>(1);
+        bTree.left = new TreeNode<>(2);
+        bTree.right = new TreeNode<>(3);
+        bTree.left.left = new TreeNode<>(4);
+        Assert.assertEquals(bTree, lowestCommonAncestorI.findLCA(bTree, bTree.left.left, bTree.right));
+    }
+
+    private void testOnlyRight() {
+        TreeNode<Integer> bTree = new TreeNode<>(1);
+        bTree.left = new TreeNode<>(2);
+        bTree.right = new TreeNode<>(3);
+        bTree.right.right = new TreeNode<>(4);
+        Assert.assertEquals(bTree, lowestCommonAncestorI.findLCA(bTree, bTree.left, bTree.right.right));
+    }
+
     private void testBST() {
         TreeNode<Integer> bst = new TreeNode<>(2);
         bst.left = new TreeNode<>(1);
@@ -57,5 +81,21 @@ public class LCAFinderTest {
         Assert.assertEquals(bst, lowestCommonAncestorI.findLCABST(bst, bst.left, bst.right));
         Assert.assertEquals(bst, lowestCommonAncestorI.findLCABST(bst, bst, bst.right));
         Assert.assertEquals(bst, lowestCommonAncestorI.findLCABST(bst, bst.left, bst));
+    }
+
+    private void testBSTLeft() {
+        TreeNode<Integer> bTree = new TreeNode<>(3);
+        bTree.left = new TreeNode<>(2);
+        bTree.right = new TreeNode<>(4);
+        bTree.left.left = new TreeNode<>(1);
+        Assert.assertEquals(bTree.left, lowestCommonAncestorI.findLCABST(bTree, bTree.left.left, bTree.left));
+    }
+
+    private void testBSTRight() {
+        TreeNode<Integer> bTree = new TreeNode<>(2);
+        bTree.left = new TreeNode<>(1);
+        bTree.right = new TreeNode<>(3);
+        bTree.right.right = new TreeNode<>(4);
+        Assert.assertEquals(bTree.right, lowestCommonAncestorI.findLCABST(bTree, bTree.right, bTree.right.right));
     }
 }
